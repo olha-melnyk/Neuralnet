b501affac3d754f0468e1b0a8b214faccecd441f
olha-melnyk <olhamelnyk@gmail.com>
2018-03-30 22:44:52 +0300

    Added data sorting

--- neuralnet_predicred.R
+++ neuralnet_predicred.R
@@ -4,15 +4,20 @@ setwd("/home/olha/dev/WDC/WDC/Neuralnet/")
 #install.packages("readxl")
 library(readxl)
 library(plyr)
-# 1. creating the initial data
+# creating the initial data
 data_source_1 <- read_excel("gl2017_source.xlsx", 1)
 data_source_2 <- read_excel("gl2017_calc.xlsx", 1)
-
 data_threats <- rbind.fill(list(data_source_1, data_source_2))
 str(data_threats)
 sapply(data_threats, typeof)
 head(data_threats)
 names(data_threats)
+
+# data sorting
+data_threats <- data_threats[order(data_threats$year, data_threats$region_abbr, data_threats$index_abbr),]
+data_threats <- subset(data_threats, data_threats$year >= '2005')
+head(data_threats)
+
 
 
 
